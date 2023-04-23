236caebebc3b3d6db1ceaddcede9b77166092c8c
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-04-23 03:32:34 +0300

    Add files via upload

---
+++ 03. Deposit Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DepositCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double depositedSum = double.Parse(Console.ReadLine());
+            int period = int.Parse(Console.ReadLine());
+            double rate = double.Parse(Console.ReadLine());
+            double sum = depositedSum + period * ((depositedSum * rate / 100) / 12);
+            Console.WriteLine(sum);
+        }
+
+    }
+}
