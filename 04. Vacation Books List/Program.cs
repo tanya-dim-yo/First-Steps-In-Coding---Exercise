a6eda3fdaabf2f14c26b1878aeae42373f2e73ae
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-04-23 03:33:50 +0300

    Add files via upload

---
+++ 04. Vacation Books List/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VacationBookList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pages = int.Parse(Console.ReadLine());
+            int pagesPerHour = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+            int hours = pages / pagesPerHour;
+            int hoursPerDay = hours / days;
+            Console.WriteLine(hoursPerDay);
+        }
+    }
+}
