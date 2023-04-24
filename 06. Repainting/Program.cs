cc38c9b7dd4d553fd743deb26fe2ace2ac0a7ef9
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-04-24 04:27:43 +0300

    Add files via upload

---
+++ 06. Repainting/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Repainting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nylon = int.Parse(Console.ReadLine());
+            int paint = int.Parse(Console.ReadLine());
+            int dissolver = int.Parse(Console.ReadLine());
+            int hours = int.Parse(Console.ReadLine());
+            double sumNylon = (nylon + 2) * 1.50;
+            double sumPaint = (paint + (paint * 10 / 100.0)) * 14.50;
+            double sumDissolver = dissolver * 5.00;
+            double sumMaterials = sumNylon + sumPaint + sumDissolver + 0.40;
+            double sumWorkers = (sumMaterials * 30 / 100.0) * hours;
+            Console.WriteLine(sumMaterials + sumWorkers);
+        }
+    }
+}
