5f004c08d6c08430c8713a8c171ca6ceaba09946
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-04-25 02:55:10 +0300

    Add files via upload

---
+++ 09. Fish Tank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Aquarium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+            double volume = length * width * height;
+            double liters = volume / 1000.0;
+            double usedSpace = liters * percent / 100.0;
+            double water = liters - usedSpace;
+            Console.WriteLine(water);
+        }
+    }
+}
