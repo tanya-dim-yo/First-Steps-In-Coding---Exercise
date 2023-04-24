594323add1d26ae2e57223b4ec1403951b2c5d92
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-04-24 04:28:43 +0300

    Add files via upload

---
+++ 07. Food Delivery/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FoodDelivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chickenMeals = int.Parse(Console.ReadLine());
+            int fishMeals = int.Parse(Console.ReadLine());
+            int vegMeals = int.Parse(Console.ReadLine());
+            double sumChickenMeals = chickenMeals * 10.35;
+            double sumFishMeals = fishMeals * 12.40;
+            double sumVegMeals = vegMeals * 8.15;
+            double sumMeals = sumChickenMeals + sumFishMeals + sumVegMeals;
+            double sumDessert = sumMeals * 20 / 100.0;
+            double totalSum = sumMeals + sumDessert + 2.50;
+            Console.WriteLine(totalSum);
+        }
+    }
+}
