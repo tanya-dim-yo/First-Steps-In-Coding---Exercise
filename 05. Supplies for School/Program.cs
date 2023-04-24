7ab96f1728ae4bfba67996e7d8e2dfad86d04430
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-04-24 04:26:36 +0300

    Add files via upload

---
+++ 05. Supplies for School/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SuppliesForSchool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int penPackages = int.Parse(Console.ReadLine());
+            int markerPackages = int.Parse(Console.ReadLine());
+            int boardCleaner = int.Parse(Console.ReadLine());
+            int discountPercentage = int.Parse(Console.ReadLine());
+            double sum = (penPackages * 5.80) + (markerPackages * 7.20) + (boardCleaner * 1.20);
+            double discount = sum * (discountPercentage / 100.0);
+            double totalSum = sum - discount;
+            Console.WriteLine(totalSum);
+        }
+    }
+}
