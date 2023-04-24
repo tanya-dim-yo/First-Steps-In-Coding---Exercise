beb9be6d70d36e27b545e8c8d9e8e7f157a93ec2
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-04-24 04:29:40 +0300

    Add files via upload

---
+++ 08. Basketball Equipment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BasketballEquipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fee = int.Parse(Console.ReadLine());
+            double snickers = fee - (fee * 40 / 100.0);
+            double equipment = snickers - (snickers * 20 / 100.0);
+            double ball = equipment / 4;
+            double accessories = ball / 5;
+            double expenses = fee + snickers + equipment + ball + accessories;
+            Console.WriteLine(expenses);
+        }
+    }
+}
